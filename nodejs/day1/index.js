029befb53bcf5de77fab43fedd340ffb219318b4
EmilMattsson <emil.emanuel@outlook.com>
2019-04-19 17:00:39 +0200

    refactoring -> removing semicolons!

--- nodejs/day1/index.js
+++ nodejs/day1/index.js
@@ -1,42 +1,42 @@
-let fs = require('fs');
+let fs = require('fs')
 
-fs.readFile('../input_data/day_1.txt', 'utf8', function(err, contents) {
-    if (err) console.log(err.message)
-    let strings = splitDataIntoArrayOfStrings(contents)
-    let numbers = parseIntArrayOfStrings(strings)
-    console.log(sumNumbers(numbers))
-    console.log(findFirstDuplicateSum(numbers))
-});
+fs.readFile('../input_data/day_1.txt', 'utf8', function (err, contents) {
+  if (err) console.log(err.message)
+  let strings = splitDataIntoArrayOfStrings(contents)
+  let numbers = parseIntArrayOfStrings(strings)
+  console.log(sumNumbers(numbers))
+  console.log(findFirstDuplicateSum(numbers))
+})
 
 function sumNumbers(input) {
-    return input.reduce((a, b) => a + b, 0);
+  return input.reduce((a, b) => a + b, 0)
 }
 
 function splitDataIntoArrayOfStrings(input) {
-    return input.split('\n');
+  return input.split('\n')
 }
 
 function parseIntArrayOfStrings(strings) {
-    let numbers = [];
-    strings.forEach(element => {
-        numbers.push(parseInt(element));
-    })
-    return numbers
+  let numbers = []
+  strings.forEach(element => {
+    numbers.push(parseInt(element))
+  })
+  return numbers
 }
 
 function findFirstDuplicateSum(numbers) {
-    let sums = [];
-    let sum = 0;
-    // sums.push(sum);
-    let noDuplicate = true;
-    while (noDuplicate) {
-        numbers.forEach(element => {
-            sum = sum + element;
-            if (sums.includes(sum)) {
-                noDuplicate = false;
-                return sum
-            }
-            sums.push(sum);
-        })
-    }
+  let sums = []
+  let sum = 0
+  // sums.push(sum)
+  let noDuplicate = true
+  while (noDuplicate) {
+    numbers.forEach(element => {
+      sum = sum + element
+      if (sums.includes(sum)) {
+        noDuplicate = false
+        return sum
+      }
+      sums.push(sum)
+    })
+  }
 }
--- nodejs/day_2/index.js
+++ nodejs/day_2/index.js
@@ -1,72 +1,72 @@
-let fs = require('fs');
+let fs = require('fs')
 
-fs.readFile('C:/dev/advent-of-code/nodejs/input_data/day_2.txt', 'utf8', function(err, contents) {
-    if (err) console.log(err.message);
-    getChecksum(contents);
-});
+fs.readFile('C:/dev/advent-of-code/nodejs/input_data/day_2.txt', 'utf8', function (err, contents) {
+  if (err) console.log(err.message)
+  getChecksum(contents)
+})
 
 function getChecksum(input) {
-  let strings = input.split('\n');
-  let doubles = 0;
-  let triplets = 0;
-  
+  let strings = input.split('\n')
+  let doubles = 0
+  let triplets = 0
+
   strings.forEach(string => {
-    let charArr = getUniqueCharactersInString(string);
-    
-    let doublesArr = [];
-    let tripletsArr = [];
+    let charArr = getUniqueCharactersInString(string)
+
+    let doublesArr = []
+    let tripletsArr = []
     charArr.forEach(char => {
-      findDoublesAndTriplets(string, char, doublesArr, tripletsArr);
-    });
-    
+      findDoublesAndTriplets(string, char, doublesArr, tripletsArr)
+    })
+
     if (doublesArr.length > 0 && tripletsArr.length > 0 && doublesArr.length === tripletsArr.length) {
-      doubles++;
-      triplets++;
+      doubles++
+      triplets++
     } else if (doublesArr.length > 0 && tripletsArr.length < 1) {
-      doubles++;
+      doubles++
     } else if (doublesArr.length < 1 && tripletsArr.length > 0) {
-      triplets++;
+      triplets++
     }
-  });
-  console.log('Doubles: %d, Triplets: %d, Doubles * Triplets: %d', doubles, triplets, (doubles * triplets));
+  })
+  console.log('Doubles: %d, Triplets: %d, Doubles * Triplets: %d', doubles, triplets, (doubles * triplets))
 }
 
 function getUniqueCharactersInString(string) {
-  let charArr = [];
-  for (let i = 0; i < string.length; i++) {
-    let char = string.charAt(i);
-    if (!charArr.includes(char)) charArr.push(char);
+  let charArr = []
+  for (let i = 0 i < string.length i++) {
+    let char = string.charAt(i)
+    if (!charArr.includes(char)) charArr.push(char)
   }
-  return charArr;
+  return charArr
 }
 
 function findDoublesAndTriplets(string, char, doublesArr, tripletsArr) {
-  let count = 0;
-  for (let i = 0; i < string.length; i++) {
-    if (char === string.charAt(i)) count++;
+  let count = 0
+  for (let i = 0 i < string.length i++) {
+    if (char === string.charAt(i)) count++
   }
   if (count === 2) {
-    doublesArr.push(char);
+    doublesArr.push(char)
   }
   if (count === 3) {
-    tripletsArr.push(char);
+    tripletsArr.push(char)
   }
 }
 
 function findStringsWhichDifferByOneChar(first, second) {
   // TODO compare if strings differ by only one and same index
   if (true) {
-    let firstChars = convertStringToCharArr(first);
-    let secondChar = convertStringToCharArr(second);
+    let firstChars = convertStringToCharArr(first)
+    let secondChar = convertStringToCharArr(second)
 
   }
 }
 
 function convertStringToCharArr(string) {
-  let charArr = [];
-  for (let i = 0; i < string.length; i++) {
-    let char = string.charAt(i);
-    charArr.push(char);
+  let charArr = []
+  for (let i = 0 i < string.length i++) {
+    let char = string.charAt(i)
+    charArr.push(char)
   }
-  return charArr;
+  return charArr
 }
